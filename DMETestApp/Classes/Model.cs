9e7ffa2de8d31329522f805dd35a9d63bb4a445c
Romann <46096700+RomannWhite@users.noreply.github.com>
2020-08-15 23:31:01 +0300

    fix

---
+++ DMETestApp/Classes/Model.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization.Json;
+using System.Collections.Generic;
+using DMETestApp.Adapter;
+using System.Linq;
+using System.IO;
+
+namespace DMETestApp.Classes
+{
+    class Model
+    {
+        static Model()
+        {
+            Data.Data RawData;
+            using (MemoryStream ms = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(DataLoader.LoadJsonString())))
+                RawData = new DataContractJsonSerializer(typeof(Data.Data)).ReadObject(ms) as Data.Data;
+            if (RawData.Results.Count > 0)
+                Users = RawData.Results.Select(r => new User(r)).ToList();
+        }
+        internal static int GetUserIndex(User user) => Users.IndexOf(user);
+        internal static User GetUser(int index) => index < Users.Count() ? Users[index] : null;
+        internal static UserListAdapter GetUserListAdapter(CustomActivity activity) => new UserListAdapter(Users, activity);
+        static List<User> Users;
+    }
+}
